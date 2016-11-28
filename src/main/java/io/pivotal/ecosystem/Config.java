6eafffcdd441db409c776c1528cdaf4d4821fb08
JaredGordon <jgordon@pivotal.io>
2016-11-28 11:05:31 -0500

    turn off health check, return some log info on startup.

---
+++ src/main/java/io/pivotal/ecosystem/Config.java
@@ -0,0 +1,26 @@
+package io.pivotal.ecosystem;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@Configuration
+public class Config implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+
+    @Override
+    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+        log.info("port: " + event.getEmbeddedServletContainer().getPort());
+        try {
+            log.info("address: " + InetAddress.getLocalHost().getHostAddress());
+            log.info("name: " + InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            log.error("oops: ", e);
+        }
+    }
+}
